d6c5118390d0b7d69657e23172a62f3032258ce5
jzelinka <jos.zelinka@email.cz>
2023-02-14 14:17:17 +0100

    Sudo change.

--- install.sh
+++ install.sh
@@ -2,15 +2,14 @@
 
 APP_PATH=`dirname "$0"`
 APP_PATH=`( cd "$APP_PATH" && pwd )`
-echo $APP_PATH
 
 # tools
-apt install -y rsync subversion nmap htop
-apt -y install cifs-utils
+sudo apt install -y rsync subversion nmap htop
+sudo apt -y install cifs-utils
 
 # editors
-apt install -y vim tmux i3
-snap install --classic code
+sudo apt install -y vim tmux i3
+sudo snap install --classic code
 
 # symlink dotvimrc 
 ln -fs $APP_PATH/dotvimrc ~/.vimrc
@@ -19,4 +18,4 @@ ln -fs $APP_PATH/dotvimrc ~/.vimrc
 ln -fs $APP_PATH/dottmux ~/.tmux.conf
 
 # latex
-apt -y install texlive-full
+sudo apt -y install texlive-full
